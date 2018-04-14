25be97f824e20309ded905008fc555de2402286e
Sijie.Sun <sunsijie@buaa.edu.cn>
2018-04-14 19:57:18 +0800

    new ui (#1)
    * new ui
    
    * fix

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,85 +1,46 @@
 <template>
-  <el-container>
-    <el-header>
-    </el-header>
-    <el-main>
-      <el-row>
-        <el-col :span="12">
-          <video id="video"></video>
-        </el-col>
-        <el-col :span="12">
-          <canvas id="snapshot"></canvas>
-        </el-col>
-      </el-row>
 
-      <el-row>
-        <el-col :span="24">
-          <el-input :value="caption">
+  <div id="container">
+      <v-toolbar color="blue">
+    <v-toolbar-title class="white--text">Undefined</v-toolbar-title>
+    <v-spacer></v-spacer>
+  </v-toolbar>
+ <video id="video"></video>
+     <v-btn fab dark large color="purple" @click="snapshot" id="play_btn">
+      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAIUSURBVGhD7Zg9SkNBFEaDoCgK0dJSsdLCVtyALkALWy1cgLgCswAtLMXCPqAbsFew08IutlYiiCC857lwb0DiT/JyZ+YF58BHhmjmfqdIZpJGJpPJdCmKYpoclGU5oU+NJgg0SYnMA9nUp0cPEzGQafOwqH8eHSj9RURA5o20yJT+W/2hd4+IgUiHh23913pD0R9FDISuyYq+pJ7Q808RAZEPcsJyVl9aLyjWl4iBzDPZYzmmW9QDCg0kYiBzS9Z0m/TQqZKIgIhwTuZ1u3TQp7KIgcgLSXs7YPjQIgYy92RDt44L891EDGTaZEFHxIG57iICInI7OCJxbgfMDCJiINIhWzouHMwKKmIgI7eDZR3rDzOiiAiIyO3gmKX/7YBNo4kYyMjtYJel3+2AzaKLGMjcEJ/bAfslExEQEc5YTmqlarBBapE7sq51qsNeSUQoL++TfZY+7xM2iipCefnkOmXp+8nFhtFEEJCzZFVH+8L+wUUo/0R2dGQYmBNMhPLvpMVyRseFgyFBRBC4JEs6JjzMdBWh/COJ/4sls11EKP9KDlmm+ZbI4KFEKC9ckLTf2+lSWYTyPqeyB/QZWITyvqeyB5TpW4TydirP6cvrA6X6EkEg3KnsAR1/FaF8+FPZA7p+K0L5eKeyBxTtEUHgisQ7lT2gd1eE8mlOZQ/o36R82lPZA8qPI5H+1/RMJvPfaTQ+AfPoJiq5n2/hAAAAAElFTkSuQmCC"></img>
+    </v-btn>
+      <v-card id="card" v-if="caption">
+          {{caption}}
+      </v-card>
+      <v-card id="card" v-if="translated">
+       {{translated}}
+        </v-card>
 
-          </el-input>
-        </el-col>
-      </el-row>
 
-      <el-row>
-        <el-col :span="24">
-          <el-button @click="snapshot">
-            拍照
-          </el-button>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="12">
-          <el-input v-model="text">
-
-          </el-input>
-        </el-col>
-
-        <el-col :span="12">
-          <el-input :value="translated" :disabled="true">
-
-          </el-input>
-        </el-col>
-      </el-row>
-      <el-row>
-        <el-col :span="12">
-          <el-button @click="translate">
-            翻译
-          </el-button>
-        </el-col>
-        <el-col :span="8">
-          <audio id="audio" controls="controls">
-
-          </audio>
-        </el-col>
-      </el-row>
-    </el-main>
-  </el-container>
+ </div>
 </template>
 
 <script>
-import api from '../api';
-import ElInput from "element-ui/packages/input/src/input";
+import api from "../api";
 
 export default {
-  components: {ElInput},
-  name: 'HelloWorld',
+  name: "HelloWorld",
   data() {
     return {
-      text: '',
-      translated: '',
+      text: "",
+      translated: "",
       lastOutput: {},
-      caption: '',
+      caption: ""
     };
   },
   mounted() {
     // Grab elements, create settings, etc.
-    const video = document.getElementById('video');
+    const video = document.getElementById("video");
 
     // Get access to the camera!
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       // Not adding `{ audio: true }` since we only want video now
-      navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+      navigator.mediaDevices.getUserMedia({ video:true }).then(stream => {
         video.src = window.URL.createObjectURL(stream);
         video.play();
       });
@@ -87,9 +48,9 @@ export default {
   },
   methods: {
     snapshot() {
-      const canvas = document.getElementById('snapshot');
-      const context = canvas.getContext('2d');
-      const video = document.getElementById('video');
+      const canvas = (this.canvas = document.createElement("canvas"));
+      const context = canvas.getContext("2d");
+      const video = document.getElementById("video");
       canvas.width = video.videoWidth;
       canvas.height = video.videoHeight;
       context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
@@ -98,37 +59,64 @@ export default {
     },
 
     translate() {
-      const audio = document.getElementById('audio');
-      api.translate(this.text, 'en-zh').then((res) => {
-        this.lastOutput = res.data;
-        this.translated = res.data.translation[0];
-        audio.src = res.data.tSpeakUrl;
-        audio.play();
-      }).catch((err) => {
-        alert(err);
-      });
+      const audio = document.createElement("audio");
+      api
+        .translate(this.text, "en-zh")
+        .then(res => {
+          this.lastOutput = res.data;
+          this.translated = res.data.translation[0];
+          audio.src = res.data.tSpeakUrl;
+          audio.play();
+        })
+        .catch(err => {
+          alert(err);
+        });
     },
 
     imageCaption() {
-      const canvas = document.getElementById('snapshot');
-      canvas.toBlob((data) => {
-        api.imageCaption(data).then((res) => {
-          this.caption = res.data.caption;
-          this.text = this.caption;
-          this.translate();
-        });
-      }, 'image/jpeg', 1);
-    },
-  },
+      this.canvas.toBlob(
+        data => {
+          api.imageCaption(data).then(res => {
+            this.caption = res.data.caption;
+            this.text = this.caption;
+            this.translate();
+          });
+        },
+        "image/jpeg",
+        1
+      );
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-  #container {
-    #snapshot {
-      width: 100% !important;
-      height: 100% !important;
-    }
+#container {
+  width: 100%;
+  display: flex;
+  flex-flow: row nowrap;
+}
+#video {
+  width: 100vw;
+}
+#play_btn {
+  position: absolute;
+  right:10px;
+  bottom: 10px;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+  img {
+    width: 50%;
+    height: 50%;
   }
+}
+#card {
+  margin: 1rem;
+  padding: 1rem;
+  display: flex;
+  flex-flow: column wrap;
+}
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import Vuetify from 'vuetify';
 import App from './App';
 import router from './router';
-
+import 'vuetify/dist/vuetify.min.css';
 Vue.config.productionTip = false;
-Vue.use(ElementUI);
+Vue.use(Vuetify,{
+  theme: {
+    primary: '#3f51b5',
+    secondary: '#b0bec5',
+    accent: '#8c9eff',
+    error: '#b71c1c'
+  }
+});
 
 /* eslint-disable no-new */
 new Vue({
