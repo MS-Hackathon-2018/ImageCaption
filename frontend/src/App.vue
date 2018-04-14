30025a118d1c816be82496114452033cbd1a2893
heymind <heymind@outlook.com>
2018-04-14 19:47:49 +0800

    new ui

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,19 +11,20 @@ export default {
 </script>
 
 <style>
-#app {
+body {
+  margin: 0;
+}
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
   height: 100vh;
   width: 100vw;
-
   #main {
     height: 100%;
     width: 100%;
   }
-}
+} */
 </style>
